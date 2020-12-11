8be2b05e1aa9bafa712174301ece7110eef477a2
emperor0penguin <joseph.menna@outlook.com>
2020-12-10 23:24:55 -0600

    Update to previous

--- main.py
+++ main.py
@@ -5,17 +5,14 @@
 
 import requests
 
-#site = requests.get('https://austin.craigslist.org/search/fua?hasPic=1&postedToday=1&max_price=1000')
-site = requests.get('https://austin.craigslist.org/fuo/d/austin-black-tall-dresser/7245175921.html')
-
-page_source = site.text.split('\n')
+site = requests.get('https://austin.craigslist.org/search/fua?hasPic=1&postedToday=1&max_price=1000')
+page_source = site.text
 
 
 def getImages(page_source):
     for line in page_source:
         if "imgList" in line:
-            images = []
-            for candidate in line.split('\"'):
-                if 'https' in candidate:
-                    images.add(candidate)
-            return images
+            return [c for c in line.split('\"') if 'https' in c]
+
+site = requests.get('https://austin.craigslist.org/fuo/d/austin-black-tall-dresser/7245175921.html')
+page_source = site.text.split('\n')
