51a83e944363d3ea68b68aa22a83a43812f33ec2
emperor0penguin <joseph.menna@outlook.com>
2020-12-10 21:39:22 -0600

    Update to previous

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import requests
 
-r = requests.get('https://xkcd.com/1906')
-r.status_code   #200 means 'OK'
+#site = requests.get('https://austin.craigslist.org/search/fua?hasPic=1&postedToday=1&max_price=1000')
+site = requests.get('https://austin.craigslist.org/fuo/d/austin-black-tall-dresser/7245175921.html')
 
+page_source = site.text.split('\n')
+
+
+#When opening a specific listing
+for line in page_source:
+    if "imgList" in line:
+        images = line.split('\"')
+        for candidate in images:
+            if 'https' not in candidate:
+                print(candidate)
+    
